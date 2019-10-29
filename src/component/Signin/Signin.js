c6fc9b62f1965490da956aba87085c3efa16a569
Byung-Jin Lee <byung2523@gmail.com>
2019-10-29 14:21:19 -0400

    deploy

--- src/component/Signin/Signin.js
+++ src/component/Signin/Signin.js
@@ -18,7 +18,7 @@ onPasswordChange = (event) => {
 	}
 
 onSubmitSignIn = () => {
-	fetch('http://localhost:3000/signin', {
+	fetch('https://glacial-taiga-15252.herokuapp.com/signin', {
 		method: 'post',
 		headers : {'Content-Type': 'application/json'},
 		body: JSON.stringify({
